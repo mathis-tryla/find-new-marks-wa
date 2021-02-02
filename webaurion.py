33638ed9fb2bcef07accca76746c28569e707429
Mathis <mathistryla@gmail.com>
2021-02-02 12:38:39 +0100

    Add marks search

--- webaurion.py
+++ webaurion.py
@@ -9,36 +9,54 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://webaurion.centralelille.fr/faces/Login.xhtml")
 
-pseudoinput = driver.find_element_by_xpath('//*[@id="username"]')
-pseudoinput.send_keys('mtryla')
+def login():
+	pseudoinput = driver.find_element_by_xpath('//*[@id="username"]')
+	pseudoinput.send_keys('mtryla')
 
-pwdinput = driver.find_element_by_xpath('//*[@id="password"]')
-pwdinput.send_keys('MWarrior62')
+	pwdinput = driver.find_element_by_xpath('//*[@id="password"]')
+	pwdinput.send_keys('MWarrior62')
 
-searchbtn = driver.find_element_by_xpath('//*[@id="j_idt27"]')
-searchbtn.click()
-
-resultsLink = driver.find_element_by_xpath('//*[@id="form:sidebar"]/div/div[2]/ul/li[2]/a')
-resultsLink.click()
-
-
-ig2iLink = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="form:sidebar"]/div/div[2]/ul/li[2]/ul/li/a'))
-)
-ig2iLink.click()
-
-mesNotesLink = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="form:sidebar"]/div/div[2]/ul/li[2]/ul/li/ul/li[1]/a'))
-)
-mesNotesLink.click()
-
-try:
-	searchbox = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="form:search-texte"]'))
-	) 
-	searchbox.send_keys(sys.argv[1])
-	searchbtn = driver.find_element_by_xpath('//*[@id="form:search"]')
+def getMyMarks():
+	searchbtn = driver.find_element_by_xpath('//*[@id="j_idt29"]')
 	searchbtn.click()
-finally:
-	pass
-	#driver.close()
+	resultsLink = driver.find_element_by_xpath('//*[@id="form:sidebar"]/div/div[2]/ul/li[2]/a')
+	resultsLink.click()
+	ig2iLink = WebDriverWait(driver, 10).until(
+			EC.element_to_be_clickable((By.XPATH, '//*[@id="form:sidebar"]/div/div[2]/ul/li[2]/ul/li/a'))
+	)
+	ig2iLink.click()
+	mesNotesLink = WebDriverWait(driver, 10).until(
+			EC.element_to_be_clickable((By.XPATH, '//*[@id="form:sidebar"]/div/div[2]/ul/li[2]/ul/li/ul/li[1]/a'))
+	)
+	mesNotesLink.click()
+
+def findMarks():
+	try:
+		searchbtn = driver.find_element_by_xpath('//*[@id="form:search"]')
+		# on renseigne un mot-clé pour rechercher une/des note(s) par matière
+		if len(sys.argv) > 1:
+			searchbox = WebDriverWait(driver, 10).until(
+				EC.presence_of_element_located((By.XPATH, '//*[@id="form:search-texte"]'))
+			) 
+			searchbox.send_keys(sys.argv[1])
+			searchbtn.click()
+			return 0
+		# on recherche une/des nouvelle(s) note(s)
+		else:
+			searchbtn.click()
+			nbNotes = WebDriverWait(driver, 10).until(
+				EC.presence_of_element_located((By.XPATH, '//*[@id="form:messages"]/div/ul/li/span'))
+			)
+			nbNotes = nbNotes.text
+			nbNotes = nbNotes.split()
+			nbNotes = nbNotes[0]
+			return nbNotes
+	finally:
+		pass
+		#driver.close()
+
+if __name__ == "__main__":
+   login()
+   getMyMarks()
+   note = findMarks() 
+
